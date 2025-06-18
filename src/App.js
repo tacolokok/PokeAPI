754abdf109e079990a6334414feeb01c4a84b835
tacolokok <ponceramosjero10@gmail.com>
2025-06-18 11:02:17 +0200

    jej

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Gen1 from './pages/Gen1';
-import Gen2 from './pages/Gen2';
-import Gen3 from './pages/Gen3';
-import Contact from './pages/Contact';
-import './index.css';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gen1" element={<Gen1 />} />
-        <Route path="/gen2" element={<Gen2 />} />
-        <Route path="/gen3" element={<Gen3 />} />
-        <Route path="/contact/:name" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
