e3a07ec65fbe5ec2ac4ba473b3a9ea31f9ca9e6f
tacolokok <167173038+tacolokok@users.noreply.github.com>
2025-06-18 11:40:24 +0200

    Update vite.config.js

--- reactasir/vite.config.js
+++ reactasir/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: './',  // <--- esta línea
   plugins: [react()],
-  base: './',
-}) 
+})
+
